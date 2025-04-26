00b48d6b87cc5c0f27f02c979fd7f74d0d44e3d6
Adiel Emilson <adielenilson@gmail.com>
2025-04-26 00:38:50 -0300

    modularizando rotas em môdulos externos

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
@@ -14,26 +15,15 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) =>{
-    res.render('home')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune: fortune.getFortune()})
-})
+app.get('/about', handlers.about)
 
 // página 404 personalizada
-app.use((req, res) => { 
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // página 500 personalizada
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http:localhost:${port}` + 'press Ctrl-C to terminate.'
