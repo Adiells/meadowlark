00b925d78618a4222e198948080dce83909566ff
Adiel Emilson <adielenilson@gmail.com>
2025-04-27 01:10:21 -0300

    utilizando eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { rules: {'no-console': 'off'},
+    files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs", globals: globals.jest} },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+]);
